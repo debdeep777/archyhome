692c131c33eceb562aae2700afc5ebe1477e844d
depY <deb@glo.com>
2014-03-25 12:11:01 +0000

    Automatic backup at Tue Mar 25 12:11:01 UTC 2014

---
+++ updateArchy.sh
@@ -0,0 +1,5 @@
+# To update my config files in archyhome repository
+nowtime=`date`
+git commit -am " Automatic backup at $nowtime"
+# Pushing to the repo that I call archyhome, which is actually debdeep777/archyhome
+git push archyhome master 
