abaa94288b131978f61aea2a0b14bf423d6c4ce9
debdeep777 <debdeep777@gmail.com>
2015-01-28 06:00:02 +0530

    Automatic backup at Wed Jan 28 06:00:01 IST 2015

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -80,6 +80,15 @@ naughty.notify({
 end
 --popup("helllooo","Theese are the texts")
 
+function arandom()
+shellcommand='ls movdown'
+local f, c_text
+f = io.popen(shellcommand)
+c_text = f:read("*all")
+f:close()
+popup("list files", c_text)
+end
+
 
 -- common
 modkey     = "Mod4"
@@ -618,6 +627,7 @@ globalkeys = awful.util.table.join(
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
+    awful.key({ modkey,           }, "p",      arandom()                                        ),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
