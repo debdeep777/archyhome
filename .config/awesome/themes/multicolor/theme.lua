1ab5a4b6133e9326d7dc5f18002c2aa89e957309
debdeep777 <debdeep777@gmail.com>
2015-02-02 00:00:01 +0530

    Automatic backup at Mon Feb  2 00:00:01 IST 2015

--- .config/awesome/themes/multicolor/theme.lua
+++ .config/awesome/themes/multicolor/theme.lua
@@ -1,124 +1,114 @@
----------------------------------
---  "Solarized" awesome theme  --
---     By Gwenhael Le Moine    --
----------------------------------
+--[[
+                                      
+     Multicolor Awesome WM config 2.0 
+     github.com/copycat-killer        
+                                      
+--]]
 
--- Alternative icon sets and widget icons:
---  * http://awesome.naquadah.org/wiki/Nice_Icons
 
--- {{{ Main
-theme = {}
-theme.default_themes_path = "/usr/share/awesome/themes"
-theme.wallpaper_cmd = { "awsetbg "..theme.default_themes_path.."/zenburn/zenburn-background.png" }
+theme                               = {}
+
+theme.confdir                       = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
+theme.wallpaper                     = theme.confdir .. "/min.jpg"
+
+theme.font                          = "DejaVu Sans Mono 10"
+--theme.taglist_font                =
+theme.menu_bg_normal                = "#000000"
+theme.menu_bg_focus                 = "#000000"
+theme.bg_normal                     = "#000000" -- zathurarc bg color #002B36
+theme.bg_focus                      = "#444444"
+theme.bg_urgent                     = "#000000"
+theme.fg_normal                     = "#aaaaaa"
+theme.fg_focus                      = "#ff8c00"
+theme.fg_urgent                     = "#af1d18"
+theme.fg_minimize                   = "#ffffff"
+theme.fg_black                      = "#424242"
+theme.fg_red                        = "#ce5666"
+theme.fg_green                      = "#80a673"
+theme.fg_yellow                     = "#ffaf5f"
+theme.fg_blue                       = "#7788af"
+theme.fg_magenta                    = "#94738c"
+theme.fg_cyan                       = "#778baf"
+theme.fg_white                      = "#aaaaaa"
+theme.fg_blu                        = "#8ebdde"
+theme.border_width                  = "1"
+theme.border_normal                 = "#000000"
+theme.border_focus                  = "#606060"
+theme.border_marked                 = "#3ca4d8"
+theme.menu_width                    = "110"
+theme.menu_border_width             = "0"
+theme.menu_fg_normal                = "#aaaaaa"
+theme.menu_fg_focus                 = "#ff8c00"
+theme.menu_bg_normal                = "#050505dd"
+theme.menu_bg_focus                 = "#050505dd"
+
 theme.colors = {}
-theme.colors.base3   = "#1c1c1cff"
-theme.colors.base2   = "#262626ff"
-theme.colors.base1   = "#585858ff"
-theme.colors.base0   = "#626262ff"
-theme.colors.base00  = "#808080ff"
-theme.colors.base01  = "#8a8a8aff"
-theme.colors.base02  = "#e4e4e4ff"
-theme.colors.base03  = "#ffffd7ff"
-theme.colors.yellow  = "#af8700ff"
-theme.colors.orange  = "#d75f00ff"
-theme.colors.red     = "#d70000ff"
-theme.colors.magenta = "#af005fff"
-theme.colors.violet  = "#5f5fafff"
-theme.colors.blue    = "#0087ffff"
-theme.colors.cyan    = "#00afafff"
-theme.colors.green   = "#5f8700ff"
+theme.colors.base3   = "#002b36"
+theme.colors.base2   = "#073642"
+theme.colors.base1   = "#586e75"
+theme.colors.base0   = "#657b83"
+theme.colors.base00  = "#839496"
+theme.colors.base01  = "#93a1a1"
+theme.colors.base02  = "#eee8d5"
+theme.colors.base03  = "#fdf6e3"
+theme.colors.yellow  = "#b58900"
+theme.colors.orange  = "#cb4b16"
+theme.colors.red     = "#dc322f"
+theme.colors.magenta = "#d33682"
+theme.colors.violet  = "#6c71c4"
+theme.colors.blue    = "#268bd2"
+theme.colors.cyan    = "#2aa198"
+theme.colors.green   = "#859900"
 -- }}}
-
--- {{{ Styles
-theme.font      = ""
-
+ 
 -- {{{ Colors
 theme.fg_normal  = theme.colors.base02
 theme.fg_focus   = theme.colors.base03
 theme.fg_urgent  = theme.colors.base3
-
+ 
 theme.bg_normal  = theme.colors.base3
 theme.bg_focus   = theme.colors.base1
 theme.bg_urgent  = theme.colors.red
 theme.bg_systray = theme.bg_normal
--- }}}
 
--- {{{ Borders
-theme.border_width  = "1"
-theme.border_normal = theme.bg_normal
-theme.border_focus  = theme.bg_focus
-theme.border_marked = theme.bg_urgent
--- }}}
+theme.menu_submenu_icon             = theme.confdir .. "/icons/submenu.png"
+theme.widget_temp                   = theme.confdir .. "/icons/temp.png"
+theme.widget_uptime                 = theme.confdir .. "/icons/ac.png"
+theme.widget_cpu                    = theme.confdir .. "/icons/cpu.png"
+theme.widget_weather                = theme.confdir .. "/icons/dish.png"
+theme.widget_fs                     = theme.confdir .. "/icons/fs.png"
+theme.widget_mem                    = theme.confdir .. "/icons/mem.png"
+theme.widget_fs                     = theme.confdir .. "/icons/fs.png"
+theme.widget_note                   = theme.confdir .. "/icons/note.png"
+theme.widget_note_on                = theme.confdir .. "/icons/note_on.png"
+theme.widget_netdown                = theme.confdir .. "/icons/net_down.png"
+theme.widget_netup                  = theme.confdir .. "/icons/net_up.png"
+theme.widget_mail                   = theme.confdir .. "/icons/mail.png"
+theme.widget_batt                   = theme.confdir .. "/icons/bat.png"
+theme.widget_clock                  = theme.confdir .. "/icons/clock.png"
+theme.widget_vol                    = theme.confdir .. "/icons/spkr.png"
 
--- {{{ Titlebars
-theme.titlebar_bg_focus  = theme.bg_focus
-theme.titlebar_bg_normal = theme.bg_normal
--- }}}
+theme.taglist_squares_sel           = theme.confdir .. "/icons/square_a.png"
+theme.taglist_squares_unsel         = theme.confdir .. "/icons/square_b.png"
 
--- {{{ Mouse finder
-theme.mouse_finder_color = theme.colors.green
--- mouse_finder_[timeout|animate_timeout|radius|factor]
--- }}}
+theme.tasklist_disable_icon         = true
+theme.tasklist_floating             = "#"  --for some reason, this is the symbol for maximized window, the symbol for floating is ^, where is that setting?
+theme.tasklist_maximized_horizontal = ""
+theme.tasklist_maximized_vertical   = ""
 
--- {{{ Menu
--- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
-theme.menu_height = "15"
-theme.menu_width  = "100"
--- }}}
+theme.layout_tile                   = theme.confdir .. "/icons/tile.png"
+theme.layout_tilegaps               = theme.confdir .. "/icons/tilegaps.png"
+theme.layout_tileleft               = theme.confdir .. "/icons/tileleft.png"
+theme.layout_tilebottom             = theme.confdir .. "/icons/tilebottom.png"
+theme.layout_tiletop                = theme.confdir .. "/icons/tiletop.png"
+theme.layout_fairv                  = theme.confdir .. "/icons/fairv.png"
+theme.layout_fairh                  = theme.confdir .. "/icons/fairh.png"
+theme.layout_spiral                 = theme.confdir .. "/icons/spiral.png"
+theme.layout_dwindle                = theme.confdir .. "/icons/dwindle.png"
+theme.layout_max                    = theme.confdir .. "/icons/max.png"
+theme.layout_fullscreen             = theme.confdir .. "/icons/fullscreen.png"
+theme.layout_magnifier              = theme.confdir .. "/icons/magnifier.png"
+theme.layout_floating               = theme.confdir .. "/icons/floating.png"
 
--- {{{ Icons
--- {{{ Taglist
-theme.taglist_squares_sel   = theme.default_themes_path.."/zenburn/taglist/squarefz.png"
-theme.taglist_squares_unsel = theme.default_themes_path.."/zenburn/taglist/squarez.png"
---theme.taglist_squares_resize = "false"
--- }}}
-
--- {{{ Misc
-theme.awesome_icon           = theme.default_themes_path.."/zenburn/awesome-icon.png"
-theme.menu_submenu_icon      = theme.default_themes_path.."/default/submenu.png"
--- }}}
-
--- {{{ Layout
-theme.layout_tile       = theme.default_themes_path.."/zenburn/layouts/tile.png"
-theme.layout_tileleft   = theme.default_themes_path.."/zenburn/layouts/tileleft.png"
-theme.layout_tilebottom = theme.default_themes_path.."/zenburn/layouts/tilebottom.png"
-theme.layout_tiletop    = theme.default_themes_path.."/zenburn/layouts/tiletop.png"
-theme.layout_fairv      = theme.default_themes_path.."/zenburn/layouts/fairv.png"
-theme.layout_fairh      = theme.default_themes_path.."/zenburn/layouts/fairh.png"
-theme.layout_spiral     = theme.default_themes_path.."/zenburn/layouts/spiral.png"
-theme.layout_dwindle    = theme.default_themes_path.."/zenburn/layouts/dwindle.png"
-theme.layout_max        = theme.default_themes_path.."/zenburn/layouts/max.png"
-theme.layout_fullscreen = theme.default_themes_path.."/zenburn/layouts/fullscreen.png"
-theme.layout_magnifier  = theme.default_themes_path.."/zenburn/layouts/magnifier.png"
-theme.layout_floating   = theme.default_themes_path.."/zenburn/layouts/floating.png"
--- }}}
-
--- {{{ Titlebar
-theme.titlebar_close_button_focus  = theme.default_themes_path.."/zenburn/titlebar/close_focus.png"
-theme.titlebar_close_button_normal = theme.default_themes_path.."/zenburn/titlebar/close_normal.png"
-
-theme.titlebar_ontop_button_focus_active  = theme.default_themes_path.."/zenburn/titlebar/ontop_focus_active.png"
-theme.titlebar_ontop_button_normal_active = theme.default_themes_path.."/zenburn/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_inactive  = theme.default_themes_path.."/zenburn/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_inactive = theme.default_themes_path.."/zenburn/titlebar/ontop_normal_inactive.png"
-
-theme.titlebar_sticky_button_focus_active  = theme.default_themes_path.."/zenburn/titlebar/sticky_focus_active.png"
-theme.titlebar_sticky_button_normal_active = theme.default_themes_path.."/zenburn/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_inactive  = theme.default_themes_path.."/zenburn/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_inactive = theme.default_themes_path.."/zenburn/titlebar/sticky_normal_inactive.png"
-
-theme.titlebar_floating_button_focus_active  = theme.default_themes_path.."/zenburn/titlebar/floating_focus_active.png"
-theme.titlebar_floating_button_normal_active = theme.default_themes_path.."/zenburn/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_inactive  = theme.default_themes_path.."/zenburn/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_inactive = theme.default_themes_path.."/zenburn/titlebar/floating_normal_inactive.png"
-
-theme.titlebar_maximized_button_focus_active  = theme.default_themes_path.."/zenburn/titlebar/maximized_focus_active.png"
-theme.titlebar_maximized_button_normal_active = theme.default_themes_path.."/zenburn/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_inactive  = theme.default_themes_path.."/zenburn/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_inactive = theme.default_themes_path.."/zenburn/titlebar/maximized_normal_inactive.png"
--- }}}
--- }}}
 
 return theme
