0cec0ed3f584986beca26553b5c41e33579dea85
Debdeep <debdeep777@gmail.com>
2017-06-30 21:09:59 -0400

    fixed auto-rotation

--- toggle-rotate.sh
+++ toggle-rotate.sh
@@ -1,42 +1,24 @@
-notify-send "Hi"
-# Auto rotate screen based on device orientation
-##########
-# Dependency: 
-###sudo apt install iio-sensor-proxy inotify-tools
-#
-# Receives input from monitor-sensor (part of iio-sensor-proxy package)
-# Screen orientation and launcher location is set based upon accelerometer position
-# Launcher will be on the left in a landscape orientation and on the bottom in a portrait orientation
-# This script should be added to startup applications for the user
 
-########################
-# /need to run 
-watch -n 4 cat '/sys/bus/iio/devices/iio:device'*/*raw* >> NULL &
+#set up the watch
+#the device acknowledges new value only when it is refreshed, e.g. through cat
+# 4 sec sounds good enough
+# only the accelerometer data, not light (device0)
+watch -n 4 cat /sys/bus/iio/devices/iio\:device1/*raw* > /dev/null 2>&1 &
 echo $!
-#as a workaround beforehand
+# Saving the output in a file
+monitor-sensor >> accel.log 2>&1 &
 
-# Clear sensor.log so it doesn't get too long over time
-> sensor.log
+#when the file is modified, take action
+ while inotifywait -e modify accel.log;
+ do 
+	 # to grab accelerator data, filter out the light data
+	 # in that case, have to add | Grep Accel beforehand
+	 # breaking the last line to find the direction
+	 direction=$(tail -n 1 accel.log | grep Accel | awk -F ': ' '{ print $2}' | awk -F '-' '{ print $1}');
 
-# Launch monitor-sensor and store the output in a variable that can be parsed by the rest of the script
-monitor-sensor >> sensor.log 2>&1 &
+	 #add a condition to check if direction in nonempty
+	 if [ -n "$direction" ];
+		 then rotate $direction; 
+	 fi
+ done
 
-# Parse output or monitor sensor to get the new orientation whenever the log file is updated
-# Possibles are: normal, bottom-up, right-up, left-up
-# Light data will be ignored
-while inotifywait -e modify sensor.log; do
-	# Read the last line that was added to the file and get the orientation
-	ORIENTATION=$(tail -n 1 sensor.log | grep 'orientation' | grep -oE '[^ ]+$')
-
-	# Set the actions to be taken for each possible orientation
-	case "$ORIENTATION" in
-		normal)
-			~/rotate normal;;
-		bottom-up)
-			~/rotate inverted;;
-		right-up)
-			~/rotate right;;
-		left-up)
-			~/rotate left;;
-	esac
-done
